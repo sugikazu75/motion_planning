832e81e2c99e5e23f8074ca16c33478f0d5b2326
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-05-09 15:01:00 +0900

    add debug parameter to 3d inverse kinematics

--- include/motion_planning/inverse_kinematics_3d.hpp
+++ include/motion_planning/inverse_kinematics_3d.hpp
@@ -10,24 +10,30 @@
 namespace motion_planning
 {
 bool solveIK(const pinocchio::Model& model, pinocchio::Data& data, const pinocchio::FrameIndex frame_id,
-             const Eigen::Vector3d& x_des, const Eigen::VectorXd& q_init, Eigen::VectorXd& q_out, int max_iter = 100,
-             double eps = 1e-4)
+             const Eigen::Vector3d& x_des, const Eigen::VectorXd& q_init, Eigen::VectorXd& q_out, bool debug = false,
+             int max_iter = 100, double eps = 1e-4)
 {
   const double DT = 1.0;
   const double damp = 1e-12;
   Eigen::VectorXd q = q_init;
+
+  // IK iteration
   for (int i = 0; i < max_iter; ++i)
   {
     pinocchio::framesForwardKinematics(model, data, q);
     Eigen::Vector3d x_curr = data.oMf[frame_id].translation();
     Eigen::Vector3d err = x_des - x_curr;  // in world frame
-    // std::cout << "Iteration " << i << std::endl;
-    // std::cout << "q = " << std::endl;
-    // std::cout << q.transpose() << std::endl;
-    // std::cout << x_curr.transpose() << std::endl;
-    // std::cout << "error:" << std::endl;
-    // std::cout << err.transpose() << std::endl;
-    // std::cout << std::endl;
+
+    if (debug)
+    {
+      std::cout << "Iteration " << i << std::endl;
+      std::cout << "q = " << std::endl;
+      std::cout << q.transpose() << std::endl;
+      std::cout << x_curr.transpose() << std::endl;
+      std::cout << "error:" << std::endl;
+      std::cout << err.transpose() << std::endl;
+      std::cout << std::endl;
+    }
 
     if (err.norm() < eps)
     {
@@ -38,33 +44,55 @@ bool solveIK(const pinocchio::Model& model, pinocchio::Data& data, const pinocch
     Eigen::MatrixXd J6 = Eigen::MatrixXd::Zero(6, model.nv);
     pinocchio::computeFrameJacobian(model, data, q, frame_id, pinocchio::WORLD, J6);  // world frame
     Eigen::MatrixXd J = J6.topRows(3).rightCols(model.njoints - 2);                   // position. universe and root
-    // std::cout << "jacobian:" << std::endl;
-    // std::cout << J << std::endl;
     Eigen::MatrixXd JJt;
     JJt.noalias() = J * J.transpose() + damp * Eigen::MatrixXd::Identity(3, 3);
-    // std::cout << "JJt" << std::endl;
-    // std::cout << JJt << std::endl;
-    Eigen::MatrixXd JJt_inv = JJt.inverse();
-    // std::cout << "JJt_inv" << std::endl;
-    // std::cout << JJt_inv << std::endl;
-
     Eigen::VectorXd v = Eigen::VectorXd::Zero(model.nv);
     v.tail(model.njoints - 2) = J.transpose() * JJt.ldlt().solve(err);
-    // std::cout << "v:" << std::endl;
-    // std::cout << v.transpose() << std::endl;
     q = pinocchio::integrate(model, q, v * DT);
+
+    if (debug)
+    {
+      std::cout << "jacobian:" << std::endl;
+      std::cout << J << std::endl;
+      std::cout << "JJt" << std::endl;
+      std::cout << JJt << std::endl;
+      std::cout << "v:" << std::endl;
+      std::cout << v.transpose() << std::endl;
+      std::cout << std::endl;
+    }
 
     // clamp joint angles to limits
     for (int i = 0; i < model.nq; ++i)
     {
       q(i) = std::max(model.lowerPositionLimit(i), std::min(q(i), model.upperPositionLimit(i)));
     }
-
-    // std::cout << std::endl;
-    // std::cout << std::endl;
-    // std::cout << std::endl;
   }
   return false;
+}
+
+void computeJointVelocityAndAcceleration(const pinocchio::Model& model, pinocchio::Data& data, const Eigen::VectorXd& q,
+                                         const Eigen::Vector3d& xdot_des, const Eigen::Vector3d& xddot_des,
+                                         const pinocchio::FrameIndex frame_id, Eigen::VectorXd& dq_out,
+                                         Eigen::VectorXd& ddq_out)
+{
+  Eigen::MatrixXd J6 = Eigen::MatrixXd::Zero(6, model.nv);
+  pinocchio::computeFrameJacobian(model, data, q, frame_id, pinocchio::WORLD, J6);  // world frame
+
+  Eigen::MatrixXd J = J6.topRows(3).rightCols(model.njoints - 2);  // position. universe and root
+  Eigen::MatrixXd JJt = J * J.transpose() + 1e-12 * Eigen::MatrixXd::Identity(3, 3);
+
+  Eigen::VectorXd dq = J.transpose() * JJt.ldlt().solve(xdot_des);
+  dq_out.tail(model.njoints - 2) = dq;
+
+  pinocchio::forwardKinematics(model, data, q, dq_out);
+  pinocchio::computeJointJacobiansTimeVariation(model, data, q, dq_out);
+
+  Eigen::MatrixXd Jdot6 = Eigen::MatrixXd::Zero(6, model.nv);
+  pinocchio::getFrameJacobianTimeVariation(model, data, frame_id, pinocchio::WORLD, Jdot6);
+  Eigen::MatrixXd Jdot = Jdot6.topRows(3).rightCols(model.njoints - 2);  // position. universe and root
+
+  Eigen::VectorXd ddq = J.transpose() * JJt.ldlt().solve(xddot_des - Jdot * dq_out);
+  ddq_out = ddq.tail(model.njoints - 2);
 }
 
 }  // namespace motion_planning
